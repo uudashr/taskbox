1a125b9ee9d3aa149a520889f89343805aad7236
Nuruddin Ashr <uudashr@gmail.com>
2022-12-06 19:14:54 +0700

    Add redux

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,25 +1,44 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-
 import Task from './Task';
+import { useDispatch, useSelector } from 'react-redux';
+import { updateTaskState } from '../lib/store';
 
-export default function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
-  const events = {
-    onPinTask,
-    onArchiveTask,
-  }
+
+
+export default function TaskList() {
+  const tasks = useSelector(state => {
+    const tasksInOrder = [
+      ...state.taskbox.tasks.filter(t => t.state === 'TASK_PINNED'),
+      ...state.taskbox.tasks.filter(t => t.state !== 'TASK_PINNED'),
+    ];
+
+    const filteredTasks = tasksInOrder.filter(t => t.state === 'TASK_INBOX' || t.state === 'TASK_PINNED')
+    return filteredTasks;
+  });
+
+  const { status } = useSelector(state => state.taskbox);
+
+  const dispatch = useDispatch();
+
+  const pinTask = (value) => {
+    dispatch(updateTaskState({ id: value, newTaskState: 'TASK_PINNED' }));
+  };
+
+  const archiveTask = (value) => {
+    dispatch(updateTaskState({ id: value, newTaskState: 'TASK_ARCHIVED' }));
+  };
 
   const LoadingRow = (
     <div className="loading-item">
         <span className="glow-checkbox"></span>
-        <span classNAme="glow-text">
+        <span className="glow-text">
           <span>Loading</span> <span>cool</span> <span>state</span>
         </span>
 
     </div>
   );
 
-  if (loading) {
+  if (status === 'loading') {
     return (
       <div className="list-items" data-testid="loading" key={"loading"}>
         {LoadingRow}
@@ -44,27 +63,16 @@ export default function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
     );
   }
 
-  const tasksInOrder = [
-    ...tasks.filter(t => t.state === "TASK_PINNED"),
-    ...tasks.filter(t => t.state !== "TASK_PINNED"),
-  ];
-
   return (
-    <div className="list-items">
-      {tasksInOrder.map(task => (
-        <Task key={task.id} task={task} {...events} />
+    <div className="list-items" data-testid="success" key={"success"}>
+      {tasks.map(task => (
+        <Task 
+          key={task.id} 
+          task={task}
+          onPinTask={(task) => pinTask(task)}
+          onArchiveTask={(task) => archiveTask(task)}
+        />
       ))}
     </div>
   );
 }
-
-TaskList.propTypes = {
-  loading: PropTypes.bool,
-  tasks: PropTypes.arrayOf(Task.propTypes.task).isRequired,
-  onPintask: PropTypes.func,
-  onArchiveTask: PropTypes.func,
-};
-
-TaskList.defaultProps = {
-  loading: false,
-};
--- src/components/TaskList.stories.js
+++ src/components/TaskList.stories.js
@@ -3,16 +3,11 @@ import React from 'react';
 import TaskList from './TaskList';
 import * as TaskStories from './Task.stories';
 
-export default {
-  component: TaskList,
-  title: 'TaskList',
-  decorators: [story => <div style={{ padding: '3rem' }}>{story()}</div>],
-};
+import { Provider } from 'react-redux';
 
-const Template = args => <TaskList {...args} />;
+import { configureStore, createSlice } from '@reduxjs/toolkit';
 
-export const Default = Template.bind({});
-Default.args = {
+export const MockedState = {
   tasks: [
     { ...TaskStories.Default.args.task, id: '1', title: 'Task 1' },
     { ...TaskStories.Default.args.task, id: '2', title: 'Task 2' },
@@ -21,24 +16,117 @@ Default.args = {
     { ...TaskStories.Default.args.task, id: '5', title: 'Task 5' },
     { ...TaskStories.Default.args.task, id: '6', title: 'Task 6' },
   ],
+  status: 'idle',
+  error: null,
 };
+
+const Mockstore = ({ taskboxState, children }) => (
+  <Provider
+    store={configureStore({
+      reducer: {
+        taskbox: createSlice({
+          name: 'taskbox',
+          initialState: taskboxState,
+          reducers: {
+            updateTaskState: (state, action) => {
+              const { id, newTaskState } = action.payload;
+              const task = state.tasks.findIndex(task => task.id === id);
+              if (task >= 0) {
+                state.tasks[task].state = newTaskState;
+              }
+            },
+          },
+        }).reducer,
+      },
+    })}
+  >
+    {children}
+  </Provider>
+);
+
+export default {
+  component: TaskList,
+  title: 'TaskList',
+  decorators: [story => <div style={{ padding: '3rem' }}>{story()}</div>],
+  excludeStories: /.*MockedState$/
+};
+
+const Template = () => <TaskList />;
+
+export const Default = Template.bind({});
+// Default.args = {
+//   tasks: [
+//     { ...TaskStories.Default.args.task, id: '1', title: 'Task 1' },
+//     { ...TaskStories.Default.args.task, id: '2', title: 'Task 2' },
+//     { ...TaskStories.Default.args.task, id: '3', title: 'Task 3' },
+//     { ...TaskStories.Default.args.task, id: '4', title: 'Task 4' },
+//     { ...TaskStories.Default.args.task, id: '5', title: 'Task 5' },
+//     { ...TaskStories.Default.args.task, id: '6', title: 'Task 6' },
+//   ],
+// };
+Default.decorators = [
+  (story) => <Mockstore taskboxState={MockedState}>{story()}</Mockstore>,
+];
 
 export const WithPinnedTasks = Template.bind({});
-WithPinnedTasks.args = {
-  tasks: [
-    ...Default.args.tasks.slice(0, 5),
-    { id: '6', title: 'Tasks 6 (pinned)', state: 'TASK_PINNED' },
-  ],
-};
+// WithPinnedTasks.args = {
+//   tasks: [
+//     ...Default.args.tasks.slice(0, 5),
+//     { id: '6', title: 'Tasks 6 (pinned)', state: 'TASK_PINNED' },
+//   ],
+// };
+WithPinnedTasks.decorators = [
+  (story) => {
+    const pinnedTasks = [
+      ...MockedState.tasks.slice(0, 5),
+      { id: '6', title: 'Task 6 (pinned)', state: 'TASK_PINNED' },
+    ];
+
+    return (
+      <Mockstore
+        taskboxState={{
+          ...MockedState,
+          tasks: pinnedTasks,
+        }}
+      >
+        {story()}
+      </Mockstore>
+    );
+  },
+];
 
 export const Loading = Template.bind({});
-Loading.args = {
-  tasks: [],
-  loading: true,
-};
+// Loading.args = {
+//   tasks: [],
+//   loading: true,
+// };
+Loading.decorators = [
+  (story) => (
+    <Mockstore
+      taskboxState={{
+        ...MockedState,
+        status: 'loading',
+      }}
+    >
+      {story()}
+    </Mockstore>
+  ),
+];
 
 export const Empty = Template.bind({});
-Empty.args = {
-  ...Loading.args,
-  loading: false,
-};
+// Empty.args = {
+//   ...Loading.args,
+//   loading: false,
+// };
+Empty.decorators = [
+  (story) => (
+    <Mockstore
+      taskboxState={{
+        ...MockedState,
+        tasks: [],
+      }}
+    >
+      {story()}
+    </Mockstore>
+  ),
+];
